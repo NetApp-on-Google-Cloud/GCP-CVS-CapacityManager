f922fbb8da272a366dfe8b50c49133ee6c6d0a85
Oliver Krause <3621164+okrause@users.noreply.github.com>
2021-09-29 14:25:44 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # GCP-CVS-CapacityManager
-Resizes Cloud Volumes Service volumes on GCP to avoid out-of-storage conditions.
+
+A script to resize [Cloud Volumes Service (CVS)](https://cloud.google.com/architecture/partners/netapp-cloud-volumes/overview?hl=en_US) volumes on GCP to avoid running into out-of-space conditions.
+
+## Concept
+Whenever the script runs, it enumerates all volumes in the specified project. For each volume, it determines the used size (numbers of bytes consumed by data) and the quota (size of the volume as set by user).
+
+The user  needs to specify the *interval* the script is invoked and an optional security *margin*.
+
+Since the service level and the size of a volume determine maximum write speed, the script can calculate the maximum amount of new data which can be written to the volume within the interval (e.g every 60 minutes). This results in a new potential size. A security margin (in %) can be added (e.g. 10 for 10%) on top. If the new potential size is bigger than the current quota, the volume quota will be grown to the calculated size.
+
+If the script is executed at the same scheduling interval specified in *duration*, the volume will always be big enough to avoid out-of-space conditions.
+
+Using Google Cloud Scheduler, PubSub and Cloud Functions, the process can be automated.
+
+## Usage
+
+The script can be invoked via CLI or via a PubSub message. Independent of invokation type, 4 arguments need to be provided:
+
+* **projectid**: GCP project ID (e.g my-project) or GCP project number (e.g. 1234567890). If project ID is specified, the script needs resourcemanager.projects.get permissions, otherwise use project number
+* **duration**: Time interval in minutes the script will be called (e.g. 60 for every 60 minutes)
+* **margin**: Additional security margin in %. The script calculates the new target size and will put margin % on top of it
+* **service_account**: A base64 encoded JSON key for a [IAM service account](https://cloud.google.com/architecture/partners/netapp-cloud-volumes/api?hl=en_US) which has **roles/netappcloudvolumes.admin**. Use
+```bash
+cat key.json | base64
+```
+to generate it. Goal is to eventually use service account impersonation, but we need to stick with passing credentials for now
+
+### CLI
+The script takes the 4 arguments via environment variables:
+
+```bash
+# Make sure python3.6 or later environment exists on your machine
+pip3 install -r requirements.txt
+export DEVSHELL_PROJECT_ID=$(gcloud config get-value project)
+export CVS_CAPACITY_INTERVAL=15 # default is 60 minutes
+export CVS_CAPACITY_MARGIN=15 # default is 20% on top
+export SERVICE_ACCOUNT_CREDENTIAL=$(cat key.json | base64)
+python3 ./main.py
+```
+
+### PubSub message
+
+The script contains a PubSub subscriber function called CVSCapacityManager_pubsub. It expects a payload like:
+
+```json
+{
+        "projectid":        "my-project",
+        "duration":         60,
+        "margin":           20,
+        "service_account":  "abcd..."
+}
+```
+
+*Note*: service_account is a JSON key encoded in base64
+
+The intended way to run it is using [Google Cloud Scheduler](https://cloud.google.com/scheduler) to trigger [Google PubSub messages](https://cloud.google.com/pubsub) which are received by this script running as [Google Cloud Function](https://cloud.google.com/functions).
+
+```bash
+# Create new PubSub topic
+topic=CVSCapacityManager
+gcloud pubsub topics create $topic
+
+# Set serviceAccount to name of service account with cloudvolumes.admin permissions (see https://cloud.google.com/architecture/partners/netapp-cloud-volumes/api?hl=en_US). This can later be used for service account impersonation, but is currently defunct.
+# Provide JSON key to this service account in a file named key.json
+# serviceAccount=$(cat key.json | jq '.client_email')
+serviceAccount="cvs-sa-svcacct@cv-solution-architect-lab.iam.gserviceaccount.com"
+
+# Deploy Cloud Function
+gcloud functions deploy CVSCapacityManager --entry-point CVSCapacityManager_pubsub --trigger-topic $topic --runtime=python39 --region=europe-west1 --service-account $serviceAccount
+
+# Setup Cloud Scheduler
+# run every hour (60 minutes), margin 20%
+IFS='' read -r -d '' payload <<EOF
+{
+        "projectid":        "$(gcloud config get-value project)",
+        "duration":         60,
+        "margin":           20,
+        "service_account":  "$(cat key.json | base64)"
+}
+EOF
+gcloud scheduler jobs create pubsub CVSCapacityManager-job --schedule="0 * * * *" --time-zone="Etc/UTC" --topic=$topic --message-body=$payload
+```
+
+## Notes
+* Only volumes with *lifeCycleState = available* are considered, all others are ignored
+* Snapshot space handling: The script uses the usedBytes API parameter. It includes all active data, metadata and snapshot blocks
+* SnapReserve handling: *snapReserve* only impacts the amount of space presented to the client via df (e.g 2000 GiB volume, snapReserve 20%, client only sees 1600 GiB). The script uses *usedBytes*, which isn't influenced by *snapReserve*
+* If target volume size is larger than [maximum volume size](https://cloud.google.com/architecture/partners/netapp-cloud-volumes/resource-limits-quotas?hl=en_US), behaviour is unknown. Will either fail or resize to maximum size. Not tested.
+* For host/service projects, run the script in each service/host project which provisioned volumes
+* Script is developed and tested with Python 3.9
+
+## Troubleshooting
+If you run into issues, try running the script via the CLI way first, as it removes the complexity of Cloud Schedule, PubSub and CloudFunctions. Does it work? Are your parameters correct?
+
+Setting 
+```bash
+export CVS_DEBUGGING=true
+```
+will output more information.
+
+If CLI works, but CloudFunction doesn't, check the payload string in Cloud Scheduler first. Does it have a projectID? Have you tried project number instead? Is the service account key empty? Is it correct (try as base64 decode to see if it returns a proper key).
+
+Are you seeing python exceptions? Most of them will return HTTP error codes which indicate infrastructure issues, usually network connection or authentication issues.
+
+## Support
+This tool is not officially supported by NetApp and provided as-is.
+Run at your own risk.
+
+### Risk assessment
+
+It only does read operations, except when growing a volume. To grow a volume, it queries a volume, changes the *quotaInBytes* parameter and does an PUT API call, which is the equivalent of changing the size in the UI.
+
+Larger volumes incur more monthly costs.
+
+### Getting help
+If you have any issues, please check logs first. When ran as Cloud Function, it will log a line for each volume to Cloud Logging. Any indication why it fails? Make sure the pass the parameters correctly.
+
+Next check the issue section of this repository to see if it is a known issue.
+
+If you still found no resolution, please consider opening an GitHub issue for this repository. Support is best-effort.
---
+++ main.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import sys
+import json
+import locale
+from os import getenv, environ
+import base64
+import requests
+import re
+from googleapiclient import discovery, errors
+from google.auth import default
+from google.auth.transport.requests import Request as googleRequest
+from google.auth.jwt import Credentials
+from google.oauth2 import service_account
+
+# Lookup Project Number for given ProjectID
+# requires resourcemanager.projects.get permissions
+def getGoogleProjectNumber(project_id: str) -> str:
+   credentials, _ = default()
+
+   service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
+
+   request = service.projects().get(projectId=project_id)
+   try:
+      response = request.execute()
+      return response["projectNumber"]
+   except errors.HttpError as e:
+      # Unable to resolve project. No permission or project doesn't exist
+      return None
+
+# Check if string is base64 encoded
+def isBase64(sb):
+        try:
+                if isinstance(sb, str):
+                        # If there's any unicode here, an exception will be thrown and the function will return false
+                        sb_bytes = bytes(sb, 'ascii')
+                elif isinstance(sb, bytes):
+                        sb_bytes = sb
+                else:
+                        raise ValueError("Argument must be string or bytes")
+                return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+        except Exception:
+                return False
+
+class BearerAuth(requests.auth.AuthBase):
+    credentials = None
+
+    def __init__(self, sa_key):
+        audience = 'https://cloudvolumesgcp-api.netapp.com'
+        # check if we got a path to a service account JSON key file
+        # or we got the key itself encoded base64
+        if isBase64(sa_key):
+            # we got an base64 encoded JSON key
+            svc_creds = service_account.Credentials.from_service_account_info(json.loads(base64.b64decode(sa_key)))
+        else:
+            # we got an file path to an JSON key file
+            svc_creds = service_account.Credentials.from_service_account_file(sa_key)
+        jwt_creds = Credentials.from_signing_credentials(svc_creds, audience=audience)
+        request = googleRequest()
+        jwt_creds.refresh(request)
+
+        self.credentials = jwt_creds
+        # self.expires_at = datetime.datetime.now() + datetime.timedelta(0, r.json()['expires_in'])
+
+    def __call__(self, r):
+        # Add expiration check here
+        r.headers["authorization"] = "Bearer " + self.credentials.token.decode('utf-8')
+        return r
+
+    def __str__(self):
+        return self.credentials.token
+
+# Class to handle CVS API calls
+class GCPCVS():
+    project: str = None
+    projectId: str = None
+    service_account: str = None
+    token: BearerAuth = None
+    baseurl: str = None
+    headers: dict = {
+                "Content-Type": "application/json"
+            }
+
+    # Initializes the object
+    # IN:
+    #   project = GCP project (projectNumber or  projectID)
+    #   service_account = either base64 encodes service account key or path to key JSON file
+    #                     service account needs cloudvolumes.admin permissions
+    def __init__(self, project: str, service_account: str):
+        # Resolve projectID to projectNumber
+        if re.match(r"[a-zA-z][a-zA-Z0-9-]+", project):
+            self.projectId = project
+            project = getGoogleProjectNumber(project)
+            if project == None:
+                raise ValueError("Cannot resolve projectId to project number. Please specify project number.")                
+        self.project = project
+        self.service_account = service_account
+
+        self.baseurl = 'https://cloudvolumesgcp-api.netapp.com/v2/projects/' + str(self.project)
+        self.token = BearerAuth(service_account)
+
+    # print some infos on the class
+    def __str__(self) -> str:
+        return f"CVS: Project: {self.project}\nService Account: {self.service_account}\n"
+
+    # returns list with dicts of all volumes in specified region ("-" for all regions)
+    def getVolumesByRegion(self, region: str) -> list:
+        logging.info(f"getVolumesByRegion {region}")
+        r = requests.get(f"{self.baseurl}/locations/{region}/Volumes", headers=self.headers, auth=self.token)
+        r.raise_for_status()
+        return r.json()
+        
+    # modify a volume
+    # pass in dict with API field to modify
+    # used by more specialized methods
+    def _modifyVolumeByVolumeID(self, region: str, volumeID: str, changes: dict) -> dict:
+        logging.info(f"modifyVolumeByVolumeID {region}, {volumeID}, {changes}")
+        # read volume
+        r = requests.get(f"{self.baseurl}/locations/{region}/Volumes/{volumeID}", headers=self.headers, auth=self.token)
+        r.raise_for_status()
+        volume = r.json()
+        for k in changes:
+            volume[k] = changes[k]
+        r = requests.put(f"{self.baseurl}/locations/{region}/Volumes/{volumeID}", headers=self.headers, auth=self.token, json=volume)
+        r.raise_for_status()
+        return r.json()
+    
+    # change size of volume
+    def resizeVolumeByVolumeID(self, region: str, volumeID: str, newSize: int):
+        logging.info(f"updateVolumeByVolumeID {region}, {volumeID}, {newSize}")
+        self._modifyVolumeByVolumeID(region, volumeID, {"quotaInBytes": newSize})
+
+    # CVS API uses serviceLevel = (basic, standard, extreme)
+    # CVS UI uses serviceLevel = (standard, premium, extreme)
+    # yes, the name "standard" has two different meaning *sic*
+    # CVS-SO uses serviceLevel = basic, storageClass = software and regional_ha=(true|false) and
+    # for simplicity reasons we translate it to serviceLevel = software
+    def translateServiceLevelAPI2UI(self, serviceLevel: str) -> str:
+        serviceLevelsAPI = {
+            "basic": "standard",
+            "standard": "premium",
+            "extreme": "extreme",
+            "software": "software"
+        }
+        if serviceLevel in serviceLevelsAPI:
+            return serviceLevelsAPI[serviceLevel]
+        else:
+            logging.warning(f"translateServiceLevelAPI2UI: Unknown serviceLevel {serviceLevel}")
+            return None
+
+    def translateServiceLevelUI2API(self, serviceLevel: str) -> str:
+        serviceLevelsUI = {
+            "standard": "basic",
+            "premium": "standard",
+            "extreme": "extreme",
+            "software": "software"
+        }
+        if serviceLevel in serviceLevelsUI:
+            return serviceLevelsUI[serviceLevel]
+        else:
+            logging.warning(f"translateServiceLevelUI2API: Unknown serviceLevel {serviceLevel}")
+            return None
+
+# Calculate new recommended size of volume
+# Since each volume got a performance limit per capacity (QoS), we can
+# calculate how quickly the free space can be consumed at max.
+# If we know the time interval this script is running, we can make the volume
+# big enough so it doesn't run out of space meanwhile
+# Parameters:
+#  size = current volume size in B
+#  serviceLevel = name of CVS serviceLevel (basic, standard, extreme, software)
+#  duration = time in minutes between script runs
+#  margin = add additional capacity security margin on top in %
+# Result:
+#  New proposed size in B, rounded up to align to full GiB
+def calculateNewCapacity(size: int, serviceLevel: str, duration: int, margin: int) -> int:
+    logging.info(f"calculateNewCapacity {size}, {serviceLevel}, {duration}, {margin}")
+    qos = { 'basic' : 16,
+            'standard': 64,
+            'extreme': 128,
+            'software': 128
+            }
+
+    if serviceLevel in qos:
+        speed = qos[serviceLevel]
+    else:
+        logging.warning(f'calculateNewCapacity: Unknown serviceType: {serviceLevel}. Using "high"')
+        speed = qos['high']
+        
+    # Calculate the new volume size
+    # Formula takes into consideration that bigger volume = more speed = quicker fill rate
+    newSize = int( -size / ( duration * 60 * speed / 1024**2 * (1 + margin / 100) - 1) )
+    # Round up to full GiB
+    newSize = int(newSize / 1024**3 + 1) * 1024**3
+
+    return newSize
+
+# Query all volumes in the project. For each volume, calculate how big it can grow within <duration>
+# considering the current used size and the max write speed determined by volumes serviceLevel
+# If calculated_size > allocted_size, resize volume to calculated_size
+def resize(project_id: str, service_account_credential: str, duration:int, margin: int, outputJSON: bool):
+    cvs = GCPCVS(project_id, service_account_credential)
+    
+    # Query all CVS volumes in project
+    allvolumes = cvs.getVolumesByRegion("-")
+    if outputJSON == False:
+        print(f'{"Name":30} {"serviceLevel":12} {"used [B]":>22} {"allocated [B]":>22} {"%used":5} {"new_allocated [B]":>22} {"Resize"}')
+
+    for volume in allvolumes:
+        name = volume["name"]
+        quota = volume["quotaInBytes"]
+        used = volume["usedBytes"]
+
+        if volume['lifeCycleState'] != 'available':
+            if outputJSON == True:
+                print(json.dumps({'severity': "INFO", 'volume': name, 'message': "Volume is not available. Skipping ..."}))
+            else:
+                print(f'{name:30} {"Volume is not available. Skipping ..."}')
+            continue
+
+        # CVS-software uses serviceLevel = "basic", which deliver 128 KiB/s/GiB
+        # CVS-performance serviceLevel = "basic" is "Standard", which delivers 16 KiB/s/GiB
+        # to distinguish both kinds of "basic", call the CVS-software one "software"
+        if volume["storageClass"] == "hardware":
+            serviceLevel = volume["serviceLevel"]
+        else:
+            serviceLevel = "software"
+  
+        # Calculate new size
+        newSize = calculateNewCapacity(used, serviceLevel, duration, margin)
+        if newSize > quota:
+            enlarge = True
+        else:
+            newSize = quota
+            enlarge = False
+
+        # Print volume list with relevant data
+        if outputJSON == True:
+            # Structured logging
+            entry = dict(
+                severity = "INFO",
+                volume = name,
+                serviceLevel = cvs.translateServiceLevelAPI2UI(serviceLevel),
+                used = used,
+                quota = quota,
+                enlarge = enlarge,
+                newSize = newSize
+            )
+            print(json.dumps(entry))
+        else:
+            print(f'{name:30} {cvs.translateServiceLevelAPI2UI(serviceLevel):12} {used:22n} {quota:22n} {round(used / quota * 100, 1):5n} {newSize:22n} {"Yes" if enlarge else ""}')
+
+        if enlarge == True:
+            # Volume needs resizing. Call API  
+            cvs.resizeVolumeByVolumeID("europe-west4", volume["volumeId"], newSize)
+
+# Receiver function for Cloud Function PubSub
+# expected payload in data:
+#   data = {
+#         "projectid":        "my-project",     # Project ID
+#         "duration":         60,               # Minutes between invocations of this script
+#         "margin":           10,               # Security margin of additional space to add in 0-100%
+#         "service_account":  "..."             # base64 encoded content of JSON key (cat json.key | base64)
+#         }
+def CVSCapacityManager_pubsub(event, context):
+    logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+
+    if 'data' in event:
+        parameters = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+        
+        try:
+            project_id = parameters['projectid']
+            duration = parameters['duration']
+            margin = parameters['margin']
+            service_account = parameters['service_account']
+        except KeyError:
+            return "Missing parameter", 400
+
+        # for service_account, only the first 10 characters are printed
+        print(json.dumps({ 'project_id': project_id, 'duration': duration, 'margin': margin, 'service_account': service_account[0:9] + "..."}))
+        resize(project_id, service_account, duration, margin, True)
+
+def CVSCapacityManager_cli():
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    if 'CVS_DEBUGGING' in environ:
+        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.WARNING)    
+
+    # Set this variables for your environment
+    project_id = getenv('DEVSHELL_PROJECT_ID', None)
+
+    if project_id == None:
+        logging.error('ProjectID not set in DEVSHELL_PROJECT_ID. Try "export DEVSHELL_PROJECT_ID=$(gcloud config get-value project)"')
+        sys.exit(1)
+    else:
+        print("INFO: Project:", project_id)
+
+    # check if file with service account credentials exists
+    service_account_credential = getenv('SERVICE_ACCOUNT_CREDENTIAL', None)
+    if service_account_credential == None:
+        logging.error('Missing service account credentials. Please set provide file path to JSON key file or provide credentials like "export SERVICE_ACCOUNT_CREDENTIAL=$(cat key.json | base64)"')
+        sys.exit(2)
+
+    # Amount of spare capacity in % (e.g. 10 = 10%) to add on top of calculated target capacity
+    margin = getenv('CVS_CAPACITY_MARGIN', 20)
+    # Tell script how often it is ran. Duration in minutes
+    duration = getenv('CVS_CAPACITY_INTERVAL', 60)
+    logging.info(f"duration: {duration} minutes, margin: {margin}%")
+
+    resize(project_id, service_account_credential, duration, margin, False)
+
+    # Testcode for running the pub_sub function manually
+    # payload = json.dumps({ 'projectid': project_id,
+    #             'duration': duration,
+    #             'margin': margin,
+    #             'service_account': service_account_credential
+    #             })
+    # event = { 'data': base64.b64encode(payload.encode('utf-8'))}
+    # CVSCapacityManager_pubsub(event, None)
+
+if __name__ == "__main__":
+    CVSCapacityManager_cli()
